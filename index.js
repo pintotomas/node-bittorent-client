2a2587667ea7174e11dbdec6f2a16c2c9dbd8b44
Tomas <tomasp834@gmail.com>
2019-08-20 17:36:18 -0300

    create tracker.js, get list of peers

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const fs = require('fs');
 const bencode = require('bencode');
+const tracker = require('./tracker');
 
 const torrent = bencode.decode(fs.readFileSync('big-buck-bunny.torrent'));
-const trackerUrl = torrent.announce.toString('utf8');
+
+tracker.getPeers(torrent, peers => {
+  console.log('list of peers: ', peers);
+});
